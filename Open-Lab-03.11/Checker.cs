fc07bc09e673c6d55e8719dc313d9e3bdbb30e30
Lucián Švancár <svancarlucian@gmail.com>
2022-10-16 22:59:27 +0200

    Add while loop if I is longer than J it show us true, else we char str for i and j and add new if , if it is a != b if shows us false

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,23 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
-        } 
+            int i = 0;
+            int j = str.Length - 1;
+            while (true)
+            {
+                if (i > j)
+                {
+                    return true;
+                }
+                char a = str[i];
+                char b = str[j];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
     }
 }
